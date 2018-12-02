4308dc081a99c85d7ce41c7e1849c94f00562b30
h-sakano <h.sakano@world-bit.co.jp>
2018-12-03 01:39:56 +0900

    Firebaseプロジェクトの初期化コードスニペットを貼り付け

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import Vue from 'vue'
+import firebase from 'firebase/app'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: 'AIzaSyAfrGwmwrla9a8flFCFmkl4FIBMZxORqdU',
+  authDomain: 'advent-calendar-2018-app-7a5d3.firebaseapp.com',
+  databaseURL: 'https://advent-calendar-2018-app-7a5d3.firebaseio.com',
+  projectId: 'advent-calendar-2018-app-7a5d3',
+  storageBucket: '',
+  messagingSenderId: '298560560632',
+}
+firebase.initializeApp(config)
 
 new Vue({
   router,
