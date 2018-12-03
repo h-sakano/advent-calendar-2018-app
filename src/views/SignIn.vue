b01b4f0fa57ad7466a55e2b9688a6275efb15ce9
h-sakano <h.sakano@world-bit.co.jp>
2018-12-03 09:54:19 +0900

    Firebase UIを使用し、サインアップ/サインインを実装

---
+++ src/views/SignIn.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="sign-in">
+    <h1 class="title is-5">Sign in</h1>
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class SignIn extends Vue {
+  private mounted () {
+    let ui = firebaseui.auth.AuthUI.getInstance()
+    if (!ui) {
+      ui = new firebaseui.auth.AuthUI(firebase.auth())
+    }
+    ui.start('#firebaseui-auth-container', {
+      signInFlow: 'redirect',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        {
+          provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          defaultCountry: 'JP',
+        },
+      ],
+      signInSuccessUrl: '/',
+    })
+  }
+}
+</script>
+
+<style scoped>
+.sign-in {
+  text-align: center;
+}
+</style>
